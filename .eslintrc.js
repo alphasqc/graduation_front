47e434c1bdd156a125d6f33db00e923f73a81b59
js179000250 <9736674+js179000250@user.noreply.gitee.com>
2022-11-23 16:29:47 +0800

    create content

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    '@vue/standard',
+    '@vue/typescript/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // 数据类型判断解除
+    '@typescript-eslint/no-inferrable-types': 'off',
+    // 单引号限制解除
+    quotes: [1, 'single']
+  }
+}
